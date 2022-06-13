7959c10c2bcf41b5b33b47b6e9e013fe679e3660
cares0 <cares00000@gmail.com>
2022-06-13 14:10:09 +0900

    Q-Type 인스턴스 사용법

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import study.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -55,12 +56,11 @@ public class QuerydslBasicTest {
     @Test
     public void startQuerydsl() {
         queryFactory = new JPAQueryFactory(em);
-        QMember m = new QMember("m");
 
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
